dc02b2dcb0fff5c06a6d6299a5bf4883994d4d30
sandeepsabu <sandeepsabu321@gmail.com>
2025-10-24 14:40:07 +0530

    aaass

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -30,7 +30,7 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
+      <div className="container portfolio__container ">
         {
           data.map(({id,image,title,github,demo})=>{
             return (
